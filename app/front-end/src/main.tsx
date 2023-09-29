500ba0d94ee102aec3ec0024d09cb6166de9dda0
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-29 17:48:33 -0300

    feat: add ToastContainer and GlobalsStyles to main.tsx

--- app/front-end/src/main.tsx
+++ app/front-end/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { GlobalsStyles } from './styles/GlobalStyles.tsx';
+import { ToastContainer } from './libs/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<GlobalsStyles />
 		<App />
+		<ToastContainer  />
 	</React.StrictMode>,
 );
