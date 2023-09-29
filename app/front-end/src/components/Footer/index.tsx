6ec0e0ec079782ae73a05dc1112156db29ef4eff
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-29 17:50:30 -0300

    feat: add Footer component

---
+++ app/front-end/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as C from './styles';
+
+const Footer = () => {
+	return (
+		<C.Footer>
+			<p>Desenvolvido por <a rel="noreferrer noopener" href="https://www.ulisses.tec.br" target='_blank' >Ulisses Silvério</a> </p>
+		</C.Footer>
+	);
+};
+
+export default Footer;
